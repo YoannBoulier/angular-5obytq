12a8391c9383ae07793b1aa33adef232b9e12469
YoannBoulier <yoann.boulier@gmail.com>
2020-01-26 18:16:11 +0100

    Markers init on map OK

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,11 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: 'accueil.component.html',
   styleUrls: [ 'accueil.component.css' ],
 })
-export class AccueilComponent {
-  lat = 40.730610;
-  lng = -73.935242;
+export class AccueilComponent implements OnInit {
+  lat = 47.09569;
+  lng = 4.6520182;
+  zoom = 6;
+  markers = [];
+
+  ngOnInit() {
+    this.getAllDestinations();
+  }
+
+  getAllDestinations()
+  {
+    // Appel à l'api pour récupérer toute les destinations possibles et les afficher sur la carte
+
+    // Stockage en dur pour les tests
+    this.markers = [
+      { lat: 48.185663, lng: -1.632535 },
+      { lat: 48.1137195, lng: -1.6335799 }
+    ];
+  }
 }
